0cdbe218a768a514a3338fa7584410d508d2d07d
Fagundes-Rayssa <164500870+Fagundes-Rayssa@users.noreply.github.com>
2025-02-21 09:33:13 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -1,6 +1,24 @@
 import random
 
-sorteio = random.randint(1, 10)
-chute = input("Digite o seu chute:")
-print(sorteio)
-print(chute)
+print("@@@ JOGO DA ADIVINHAÇÃO @@@")
+print("Adivinhe o número que estou pensando, de 1 a 10")
+sorteio = random.randint(1,10)
+
+limite_tentativas = 3
+tentativa = 1
+while (limite_tentativas >= tentativa):
+    print("tentativa", tentativa)
+   
+    chute = int(input("Digite o seu chute:"))
+
+
+    if (chute == sorteio):
+        print("Parabéns, você acertou o número era", sorteio)
+        break
+    elif (chute > sorteio):
+        print("Chute um numero menor!")
+    elif (chute < sorteio):
+        print("Chute um numero maior")
+    
+    
+    tentativa = tentativa + 1 
